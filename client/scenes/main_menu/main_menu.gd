caf5cd57fb22c49ac31eb1da19009aa4a1275f64
4lePl4y <aletheplayer2@gmail.com>
2026-04-09 14:18:33 +0200

    fix [main]: error with paths

--- client/scenes/main_menu/main_menu.gd
+++ client/scenes/main_menu/main_menu.gd
@@ -5,7 +5,7 @@ func _ready():
 	
 func _on_createGame_button_pressed() -> void:
 	NetworkManager.send_to_server("CREATE")
-	get_tree().change_scene_to_file("res://scenes/rooms/lobby/Lobby.tscn")
+	get_tree().change_scene_to_file("res://scenes/lobby/Lobby.tscn")
 	
 func _on_joinGame_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu/join_menu/join_menu.tscn")
