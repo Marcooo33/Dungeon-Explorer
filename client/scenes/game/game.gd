14e525e8fab5d26ad3d88e57df1dba4de52763ad
4lePl4y <aletheplayer2@gmail.com>
2026-04-30 17:34:00 +0200

    fix [main]: fixed godot error regarding rooms

--- client/scenes/game/game.gd
+++ client/scenes/game/game.gd
@@ -3,13 +3,32 @@ extends Node2D
 @onready var room_container = $RoomContainer
 
 @onready var starting_room = preload("res://scenes/dungeon/rooms/starting_room.tscn")
-@onready var room_1 = preload("res://scenes/dungeon/rooms/room_1.tscn")
-@onready var room_2 = preload("res://scenes/dungeon/rooms/room_2.tscn")
-@onready var room_3 = preload("res://scenes/dungeon/rooms/room_3.tscn")
-@onready var room_4 = preload("res://scenes/dungeon/rooms/room_4.tscn")
+# 1 door rooms
+@onready var room_n = preload("res://scenes/dungeon/rooms/1_door_rooms/room_n.tscn")
+@onready var room_s = preload("res://scenes/dungeon/rooms/1_door_rooms/room_s.tscn")
+@onready var room_e = preload("res://scenes/dungeon/rooms/1_door_rooms/room_e.tscn")
+@onready var room_w = preload("res://scenes/dungeon/rooms/1_door_rooms/room_w.tscn")
+
+# 2 doors rooms
+@onready var room_ns = preload("res://scenes/dungeon/rooms/2_doors_rooms/room_ns.tscn")
+@onready var room_ne = preload("res://scenes/dungeon/rooms/2_doors_rooms/room_ne.tscn")
+@onready var room_nw = preload("res://scenes/dungeon/rooms/2_doors_rooms/room_nw.tscn")
+@onready var room_se = preload("res://scenes/dungeon/rooms/2_doors_rooms/room_se.tscn")
+@onready var room_sw = preload("res://scenes/dungeon/rooms/2_doors_rooms/room_sw.tscn")
+@onready var room_ew = preload("res://scenes/dungeon/rooms/2_doors_rooms/room_ew.tscn")
+
+# 3 doors rooms
+@onready var room_nse = preload("res://scenes/dungeon/rooms/3_doors_rooms/room_nse.tscn")
+@onready var room_nsw = preload("res://scenes/dungeon/rooms/3_doors_rooms/room_nsw.tscn")
+@onready var room_new = preload("res://scenes/dungeon/rooms/3_doors_rooms/room_new.tscn")
+@onready var room_sew = preload("res://scenes/dungeon/rooms/3_doors_rooms/room_sew.tscn")
+
+# 4 doors room
+@onready var room_nsew = preload("res://scenes/dungeon/rooms/4_doors_room/room_nsew.tscn")
+
 @onready var player_scene = preload("res://characters/player/player.tscn")
 
-# Called when the node enters the scene tree for the first time.
+# Called when the node enters the scene tree for the first time.a
 func _ready() -> void:
 	var room_instance = starting_room.instantiate()
 	room_container.add_child(room_instance)
